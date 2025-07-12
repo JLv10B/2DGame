5bcbc84e33ecfdef3cbd78741c86e357cd92b81b
James <jamesliaw10@gmail.com>
2025-07-12 12:38:12 -0700

    initiated LevelEditor state class

---
+++ game/src/main/java/com/practice/gamestates/LevelEditor.java
@@ -0,0 +1,55 @@
+package com.practice.gamestates;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import com.practice.Game;
+import com.practice.utilz.ImageLibrary;
+
+public class LevelEditor extends State implements Statemethods {
+
+    public LevelEditor(Game game, ImageLibrary imageLibrary) {
+        super(game, imageLibrary);
+        
+    }
+
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+}
